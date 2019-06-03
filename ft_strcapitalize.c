b1be5528c2323bae0117560c0d22dec79077c453
Vuyo KOSI <vkosi@c4r15s11.wethinkcode.co.za>
2019-06-03 17:34:43 +0200

    update234

--- ft_strcapitalize.c
+++ ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: vkosi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 13:17:27 by vkosi             #+#    #+#             */
-/*   Updated: 2019/06/03 16:27:24 by vkosi            ###   ########.fr       */
+/*   Updated: 2019/06/03 17:34:36 by vkosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,69 +26,4 @@ char	*ft_strcapitalize(char *s)
 			s[i] = ft_toupper(s[i]);
 	}
 	return (s);
-}
-
-int	ft_islower(int c)
-{
-	if (c >= 97 && c <= 122)
-		return (0);
-	else
-		return(1);
-}
-
-int	ft_isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-		return (0);
-	else
-		return(1);
-}
-
-int	ft_tolower(int c)
-{
-	if (ft_isupper(c))
-		return (c + 32);
-	return (c);
-}
-
-int	ft_toupper(int c)
-{
-	if (ft_islower(c))
-		return (c - 32);
-	return (c);
-}
-
-int	ft_isalnum(int c)
-{
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
-	|| (c >= '0' && c <= '9'))
-		return (1);
-	else
-		return (0);
-}
-
-int main(int ac, char **av)
-{
-	if (ac != 2)
-		printf("\n");
-	return (0);
-	
-	int i;
-	int b;
-
-	i = 0;
-	b = av[1][i];
-	while (b != '\0')
-	{
-		if (av[1][i] >= 'a' || av[1][i] <= 'z')
-			ft_strcapitalize(&av[1][i]);
-		printf(%s"av[1][i]\n");
-		i++;
-		if (av[1][i] >= 'A' || av[1][i] <= 'Z')
-			ft_strcapitalize(&av[1][i]);
-		printf(%d"av[1][i]\n");
-		i++;
-		return (0);
-	}
-	printf("\n");
 }
