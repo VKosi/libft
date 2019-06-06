d05c69cd9108314b89d80d52fd268c7a2bd974f6
unknown <vkosi@student.wethinkcode.co.za>
2019-06-06 02:17:45 +0200

    nice

---
+++ ft_dup.c
@@ -0,0 +1,17 @@
+
+#include "libft.h"
+
+void	*ft_dup(void const *content, size_t content_size)
+{
+	unsigned char	*ptr;
+	unsigned char	*ptr2;
+	void			*new;
+
+	if (!(new = ft_memalloc(content_size)))
+		return (NULL);
+	ptr = (unsigned char*)content;
+	ptr2 = (unsigned char*)new;
+	while (*ptr)
+		*(ptr2++) = *(ptr++);
+	return (new);
+}
