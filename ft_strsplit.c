6366fcc8d6a27b26d86b093a570875af3100344d
Event WeThinkCode <event@c4r7s1.wethinkcode.co.za>
2019-06-26 17:09:24 +0200

    finalising

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: vkosi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 16:39:45 by vkosi             #+#    #+#             */
-/*   Updated: 2019/06/26 14:22:30 by vkosi            ###   ########.fr       */
+/*   Updated: 2019/06/26 17:08:50 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 
 static size_t	ft_word_count(char const *s, char c)
 {
-    int			i;
-    size_t		len;
-    i = 0;
-    len = 0;
-    while (s[i])
-    {
-        while (s[i] && s[i] == c)
-            i++;
-        if (s[i])
-        {
-            while (s[i] && s[i] != c)
+	int		i;
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			while (s[i] && s[i] != c)
 				i++;
 			len++;
 		}
@@ -35,26 +36,28 @@ static size_t	ft_word_count(char const *s, char c)
 
 char			**ft_strsplit(char const *s, char c)
 {
-    char        **tab;
-    size_t		len;
-    int			i;
-    int			begin;
-    if (!s || !(tab = (char **)malloc(sizeof(char *) * ft_word_count(s, c) + 1)))
-        return (NULL);
-    i = 0;
-    len = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-            i++;
-        else
-        {
-            begin = i;
-            while (s[i] && s[i] != c)
-                i++;
-            tab[len++] = ft_strsub(s, begin, i - begin);
-        }
-    }
-    tab[len] = (NULL);
-    return (tab);
+	char		**tab;
+	size_t		len;
+	int			i;
+	int			begin;
+
+	if (!s || !(tab = (char **)malloc(sizeof(char *) *
+	ft_word_count(s, c) + 1)))
+		return (NULL);
+	i = 0;
+	len = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			begin = i;
+			while (s[i] && s[i] != c)
+				i++;
+			tab[len++] = ft_strsub(s, begin, i - begin);
+		}
+	}
+	tab[len] = (NULL);
+	return (tab);
 }
